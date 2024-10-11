e42397824d62ba6e2726f159fe0c86a5ebbec7c9
Gabriel Borges <gabrielgbs15@gmail.com>
2024-10-11 07:27:33 -0300

    Mover imagem para whatsapp

---
+++ projetos_pyautogui/desafio2.py
@@ -0,0 +1,7 @@
+# Envio de imagens pelo whats
+
+import pyautogui
+
+pyautogui.moveTo(2135,258,duration=1)
+pyautogui.dragTo(3315,446,button='left',duration=1.5)
+pyautogui.click(3415,759,duration=1.5)
