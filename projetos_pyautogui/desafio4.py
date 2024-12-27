2c8773108739174f038facbb3cdfb151ee3dfc30
Gabriel Borges <gabrielgbs15@gmail.com>
2024-12-27 07:39:39 -0300

    arrumei

--- projetos_pyautogui/desafio4.py
+++ projetos_pyautogui/desafio4.py
@@ -1,7 +1,7 @@
 import pyautogui
 import pyperclip
 
-def escrver_frase(frase):
+def escrever_frase(frase):
     pyperclip.copy(frase)
     pyautogui.hotkey('ctrl','v')
 
@@ -10,4 +10,4 @@ pyautogui.moveTo(746,93,duration=1)
 # Clicar no locar onde vai ser colocado o texto
 pyautogui.click()
 # Escrever a mensagem
-escrver_frase('Altomação é Imcrível!')
+escrever_frase('Altomação é Imcrível!')
