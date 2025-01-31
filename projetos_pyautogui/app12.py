616a2e9f69e6ba8c68362c1b4c45cc4c9f966993
Gabriel Borges <gabrielgbs15@gmail.com>
2025-01-31 07:35:43 -0300

    Como navegar até um site

---
+++ projetos_pyautogui/app12.py
@@ -0,0 +1,9 @@
+# Como navegar até um site 
+import webbrowser
+
+# Abrir o navegador 
+webbrowser.open('https://google.com')
+# Abrir uma nova aba no navegador 
+webbrowser.open_new_tab('https://caprem.com.br')
+# Abrir uma nova janela
+webbrowser.open_new('https://youtube.com')
